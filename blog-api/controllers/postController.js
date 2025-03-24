e315247af28625ed52cef12acd66890f9ea4b9a0
vickie005 <mwendwavictory29@gmail.com>
2025-03-24 20:06:16 +0300

    enhanced with author access only

--- blog-api/controllers/postController.js
+++ blog-api/controllers/postController.js
@@ -3,7 +3,10 @@ const Post = require('../models/Post');
 // Create Post
 exports.createPost = async (req, res, next) => {
   try {
-    const post = await Post.create(req.body);
+    const post = await Post.create({
+      ...req.body,
+      author: req.user.userId,
+    });
     res.status(201).json(post);
   } catch (err) {
     next(err);
@@ -34,7 +37,17 @@ exports.getPost = async (req, res, next) => {
 // Update Post
 exports.updatePost = async (req, res, next) => {
   try {
-    const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    //Author-only check
+    if (post.author.toString() !== req.user, userId) {
+      return res.status(404).json({ message: "You are not allowed to edit this post" });
+    }
+    post.tittle = req.body.title || post.tittle;
+    post.content = req.body.content || post.content;
+    await post.save();
+
     res.status(200).json(post);
   } catch (err) {
     next(err);
@@ -44,8 +57,15 @@ exports.updatePost = async (req, res, next) => {
 // Delete Post
 exports.deletePost = async (req, res, next) => {
   try {
-    const post = await Post.findByIdAndDelete(req.params.id);
+    const post = await Post.findById(req.params.id);
     if (!post) return res.status(404).json({ message: "Post not found" });
+
+    // author only check
+    if (post.author.toString() !== req.user.userId) {
+      return res.status(403).json({ message: "You are not allowed to delete this post" });
+    }
+
+    await post.remove();
     res.status(200).json({ message: "Post deleted" });
   } catch (err) {
     next(err);
--- blog-api/routes/posts.js
+++ blog-api/routes/posts.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
 const { createPostValidator } = require('../middlewares/validators/validate');
+const validate = require('../middlewares/validators/validate');
+const postController = require('../controllers/postController');
 
-router.post('/posts', createPostValidator, validate, (req, res) => {
-    const { title, content, author } = req.body;
+// create post - protected
+router.post('/', authMiddleware, createPostValidator, validate, postController.createPost);
 
-    res.status(201).json({ message: 'Post created successfully', post: req.body });
-});
+// update post - protected
+router.put('/:id', authMiddleware, updatePostValidator, validate, postController.updatePost);
+
+//delete post - protected
+router.delete('/:id', authMiddleware, postController.deletePost);
+
+// get all posts (public)
+router.get('/:id', postController.getAllPosts);
+
+//get single post (public)
+router.get('/:id', postController.getPostById);
+
+module.exports = router;
